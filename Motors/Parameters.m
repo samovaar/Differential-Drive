5ca9ff3eb0b21ec13c521f43c2af13e3532ca48f
samovaar <ali89.05@gmail.com>
2024-07-17 11:49:19 -0700

    Added paramter struct

---
+++ Motors/Parameters.m
@@ -0,0 +1,20 @@
+Motor = struct;
+Motor.GearRatio = 15.25;
+
+Wheel = struct;
+Wheel.WheelRadius_mm = 14;
+
+
+MotorDriver = struct;
+MotorDriver.STBY_ON = 1;
+MotorDriver.STBY_OFF = 0;
+MotorDriver.AIN = [0 1];
+MotorDriver.BIN = [0 1];
+
+
+Encoder = struct;
+Encoder.CountsPerRev = 12;
+Encoder.RightChannelA = 26;
+Encoder.RightChannelB = 25;
+Encoder.LeftChannelA = 0;
+Encoder.LeftChannelB = 0;
