267a7b715f197e31f807832f1be0ed1f5aee3a00
samovaar <ali89.05@gmail.com>
2024-09-18 22:39:19 -0700

    Measured PWM from set of measured RPM

--- SpeedControl/SomePlottingData.m
+++ SpeedControl/SomePlottingData.m
@@ -1,8 +1,8 @@
-dutyCycle_pct = [30 60 90 120 150 180 210 240]; % x-asis
-RPM_value = [38 107 175 252 330 413 500 590];   % y-axis
+PWM_values = [30 60 90 120 150 180 210 240]; % x-asis
+RPM_values = [38 107 175 252 330 413 500 590];   % y-axis
 
 
-plot(dutyCycle_pct, RPM_value);
+plot(PWM_values, RPM_values);
 grid on;
 xlabel("Duty Cycle");
 ylabel("Motor RPM");
